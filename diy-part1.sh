16cca414e1a1f97479a950451283d13bc0ed623d
DGK <58463222+kel2019@users.noreply.github.com>
2020-07-05 13:52:18 +0800

    update pw

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/esirplayground/Lienol-obsolete.git package/lienol
+# git clone https://github.com/esirplayground/Lienol-obsolete.git package/lienol
